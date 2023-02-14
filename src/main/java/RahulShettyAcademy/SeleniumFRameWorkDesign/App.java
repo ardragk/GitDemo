3b306bcf3e7d755f59ea1b8c9b6f59b147f45658
ardragk <ardragk@gmail.com>
2023-02-13 16:27:50 -0800

    first

---
+++ src/main/java/RahulShettyAcademy/SeleniumFRameWorkDesign/App.java
@@ -0,0 +1,13 @@
+package RahulShettyAcademy.SeleniumFRameWorkDesign;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
