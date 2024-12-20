f41fd05c3b57859a65044c3729d96f76114c57ce
ardragk <ardragk@gmail.com>
2024-12-19 16:02:08 -0800

    this is commity from ardra

--- src/main/java/RahulShettyAcademy/SeleniumFRameWorkDesign/App.java
+++ src/main/java/RahulShettyAcademy/SeleniumFRameWorkDesign/App.java
@@ -1,13 +1,11 @@
 package RahulShettyAcademy.SeleniumFRameWorkDesign;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("Nakshatra");
     }
 }
